6fadb57283591746ec6b438263e371e0613d2104
PC <m.ikbaldogancay@hotmail.com>
2022-11-05 17:15:42 +0300

    05.11.22

---
+++ src/giriş/JavayaBaşlangıç.java
@@ -0,0 +1,10 @@
+package giriş;
+
+public class JavayaBaşlangıç {
+
+    public static void main(String[] args) {
+        System.out.println("ben yakışıklıyım");
+    }
+
+
+}
---
+++ src/giriş/ProBas.java
@@ -0,0 +1,11 @@
+package giriş;
+
+public class ProBas {
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+}
